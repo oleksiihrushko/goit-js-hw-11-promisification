73980c29342220e85ac90cc8d321c1802126f454
hrushkooleksii <hrushkooleksii@gmail.com>
2020-05-19 12:09:56 +0300

    complete hw-11 promisification

---
+++ src/index.js
@@ -0,0 +1,72 @@
+/////////////////////////////////////////////  TASK-01  ///////////////////////////////////////////////////////
+
+const delay = ms => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(ms);
+    }, ms);
+  });
+};
+
+const logger1 = time => console.log(`Resolved after ${time}ms`);
+
+delay(2000).then(logger1); // Resolved after 2000ms
+delay(1000).then(logger1); // Resolved after 1000ms
+delay(1500).then(logger1); // Resolved after 1500ms
+
+/////////////////////////////////////////////  TASK-02  ///////////////////////////////////////////////////////
+
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+const toggleUserState = (allUsers, userName) =>
+  Promise.resolve(
+    allUsers.map(user =>
+      user.name === userName ? { ...user, active: !user.active } : user,
+    ),
+  );
+
+const logger2 = updatedUsers => console.table(updatedUsers);
+
+toggleUserState(users, 'Mango').then(logger2);
+toggleUserState(users, 'Lux').then(logger2);
+
+/////////////////////////////////////////////  TASK-03  ///////////////////////////////////////////////////////
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const makeTransaction = transaction => {
+  return new Promise((resolve, reject) => {
+    const delay = randomIntegerFromInterval(200, 500);
+    setTimeout(() => {
+      const canProcess = Math.random() > 0.3;
+
+      if (canProcess) {
+        resolve({ id: transaction.id, time: delay });
+      }
+      reject(transaction.id);
+    }, delay);
+  });
+};
+
+const logSuccess = ({ id, time }) => {
+  console.log(`Transaction ${id} processed in ${time}ms`);
+};
+
+const logError = id => {
+  console.warn(`Error processing transaction ${id}. Please try again later.`);
+};
+
+makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
+
+makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
+
+makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
+
+makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
